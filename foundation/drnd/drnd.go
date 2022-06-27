10dcb3db121aefe6b3a2bd0b2adcbe3d06b2b29e
William Kennedy <bill@ardanlabs.com>
2022-06-27 10:42:48 -0500

    first level of refactoring for separating dek and data

--- foundation/drnd/drnd.go
+++ foundation/drnd/drnd.go
@@ -4,56 +4,74 @@ import (
 	"context"
 	"crypto/rand"
 	"crypto/sha256"
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"io"
-	"net"
-	"net/http"
 	"time"
 
 	"github.com/drand/drand/chain"
 	"github.com/drand/drand/client"
-	dhttp "github.com/drand/drand/client/http"
+	"github.com/drand/kyber"
 	bls "github.com/drand/kyber-bls12381"
 	"github.com/drand/kyber/encrypt/ibe"
 	"github.com/drand/kyber/pairing"
 )
 
+// Network represents a network that is used to encrypt and decrypt a DEK
+// (Data Encryption Key) for use in encrypting and decrypting messages.
+type Network interface {
+	Host() string
+	ChainHash() string
+	Client(ctx context.Context) (client.Client, error)
+	PublicKey(ctx context.Context) (kyber.Point, error)
+}
+
+// =============================================================================
+
 // EncryptWithRound will encrypt the plaintext that can only be decrypted in the
 // future specified round.
-func EncryptWithRound(ctx context.Context, out io.Writer, in io.Reader, network string, chainHash string, round uint64, armor bool) error {
-	ni, err := retrieveNetworkInfo(ctx, network, chainHash)
+func EncryptWithRound(ctx context.Context, out io.Writer, in io.Reader, network Network, round uint64, armor bool) error {
+	client, err := network.Client(ctx)
 	if err != nil {
-		return fmt.Errorf("network info: %w", err)
+		return fmt.Errorf("network client: %w", err)
 	}
 
-	roundData, err := ni.client.Get(ctx, round)
+	roundData, err := client.Get(ctx, round)
 	if err != nil {
 		return fmt.Errorf("client get round: %w", err)
 	}
 
-	return encrypt(out, in, ni, chainHash, roundData.Round(), roundData.Signature(), armor)
+	publicKey, err := network.PublicKey(ctx)
+	if err != nil {
+		return fmt.Errorf("public key: %w", err)
+	}
+
+	return encrypt(out, in, publicKey, network.ChainHash(), roundData.Round(), roundData.Signature(), armor)
 }
 
 // EncryptWithDuration will encrypt the plaintext that can only be decrypted in the
 // future specified duration.
-func EncryptWithDuration(ctx context.Context, out io.Writer, in io.Reader, network string, chainHash string, duration time.Duration, armor bool) error {
-	ni, err := retrieveNetworkInfo(ctx, network, chainHash)
+func EncryptWithDuration(ctx context.Context, out io.Writer, in io.Reader, network Network, duration time.Duration, armor bool) error {
+	client, err := network.Client(ctx)
 	if err != nil {
-		return fmt.Errorf("network info: %w", err)
+		return fmt.Errorf("network client: %w", err)
 	}
 
-	roundIDHash, roundID, err := calculateRound(duration, ni)
+	roundIDHash, roundID, err := calculateRound(duration, client)
 	if err != nil {
 		return fmt.Errorf("calculate future round: %w", err)
 	}
 
-	return encrypt(out, in, ni, chainHash, roundID, roundIDHash, armor)
+	publicKey, err := network.PublicKey(ctx)
+	if err != nil {
+		return fmt.Errorf("public key: %w", err)
+	}
+
+	return encrypt(out, in, publicKey, network.ChainHash(), roundID, roundIDHash, armor)
 }
 
 // encrypt provides base functionality for all encryption operations.
-func encrypt(out io.Writer, in io.Reader, ni networkInfo, chainHash string, roundID uint64, roundSignature []byte, armor bool) error {
+func encrypt(out io.Writer, in io.Reader, publickKey kyber.Point, chainHash string, roundID uint64, roundSignature []byte, armor bool) error {
 	suite, err := retrievePairingSuite()
 	if err != nil {
 		return fmt.Errorf("pairing suite: %w", err)
@@ -70,7 +88,7 @@ func encrypt(out io.Writer, in io.Reader, ni networkInfo, chainHash string, roun
 		return fmt.Errorf("random key: %w", err)
 	}
 
-	cipherDEK, err := ibe.Encrypt(suite, ni.chain.PublicKey, roundSignature, dek)
+	cipherDEK, err := ibe.Encrypt(suite, publickKey, roundSignature, dek)
 	if err != nil {
 		return fmt.Errorf("encrypt dek: %w", err)
 	}
@@ -92,128 +110,106 @@ func encrypt(out io.Writer, in io.Reader, ni networkInfo, chainHash string, roun
 	return nil
 }
 
-// Decrypt reads the ciphertext from the encrypted tle source and returns the
-// original plaintext.
-func Decrypt(ctx context.Context, out io.Writer, in io.Reader, network string) error {
-	fileInfo, err := read(in)
+// Decrypt reads the ciphertext from the encrypted tle source and writes the
+// original data to the output.
+func Decrypt(ctx context.Context, out io.Writer, in io.Reader, network Network) error {
+	file, err := read(in)
 	if err != nil {
 		return fmt.Errorf("decode: %w", err)
 	}
 
-	ni, err := retrieveNetworkInfo(ctx, network, fileInfo.metadata.chainHash)
-	if err != nil {
-		return fmt.Errorf("network info: %w", err)
-	}
-
-	suite, err := retrievePairingSuite()
-	if err != nil {
-		return fmt.Errorf("pairing suite: %w", err)
-	}
-
-	clientResult, err := ni.client.Get(ctx, fileInfo.metadata.roundID)
-	if err != nil {
-		if errors.Is(err, io.EOF) {
-			return errors.New("too early to decrypt")
-		}
-		return fmt.Errorf("client get round: %w", err)
-	}
-
-	var dekSignature bls.KyberG2
-	if err := dekSignature.UnmarshalBinary(clientResult.Signature()); err != nil {
-		return fmt.Errorf("unmarshal kyber G2: %w", err)
-	}
-
-	var dekKyberPoint bls.KyberG1
-	if err := dekKyberPoint.UnmarshalBinary(fileInfo.dek.kyberPoint); err != nil {
-		return fmt.Errorf("unmarshal kyber G1: %w", err)
-	}
-
-	dekCipherText := ibe.Ciphertext{
-		U: &dekKyberPoint,
-		V: fileInfo.dek.cipherV,
-		W: fileInfo.dek.cipherW,
-	}
-
-	dek, err := ibe.Decrypt(suite, ni.chain.PublicKey, &dekSignature, &dekCipherText)
+	plainDEK, err := DecryptDEK(ctx, file.cipherDEK, network, file.metadata.roundID)
 	if err != nil {
 		return fmt.Errorf("decrypt dek: %w", err)
 	}
 
-	plaintext, err := aeadDecrypt(dek, fileInfo.cipherText)
+	plainData, err := DecryptData(plainDEK, file.cipherData)
 	if err != nil {
-		return fmt.Errorf("decrypt data: %w", err)
+		return fmt.Errorf("decrypt: %w", err)
 	}
 
-	if _, err := out.Write(plaintext); err != nil {
+	if _, err := out.Write(plainData); err != nil {
 		return fmt.Errorf("write data: %w", err)
 	}
 
 	return nil
 }
 
+// DecryptDEK attempts to decrypt an encrypted DEK against the provided network
+// for the specified round.
+func DecryptDEK(ctx context.Context, cipherDEK cipherDEK, network Network, roundID uint64) (plainDEK []byte, err error) {
+	client, err := network.Client(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("network client: %w", err)
+	}
+
+	suite, err := retrievePairingSuite()
+	if err != nil {
+		return nil, fmt.Errorf("pairing suite: %w", err)
+	}
+
+	clientResult, err := client.Get(ctx, roundID)
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			return nil, errors.New("too early to decrypt")
+		}
+		return nil, fmt.Errorf("client get round: %w", err)
+	}
+
+	var dekSignature bls.KyberG2
+	if err := dekSignature.UnmarshalBinary(clientResult.Signature()); err != nil {
+		return nil, fmt.Errorf("unmarshal kyber G2: %w", err)
+	}
+
+	var dekKyberPoint bls.KyberG1
+	if err := dekKyberPoint.UnmarshalBinary(cipherDEK.kyberPoint); err != nil {
+		return nil, fmt.Errorf("unmarshal kyber G1: %w", err)
+	}
+
+	publicKey, err := network.PublicKey(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("public key: %w", err)
+	}
+
+	dek := ibe.Ciphertext{
+		U: &dekKyberPoint,
+		V: cipherDEK.cipherV,
+		W: cipherDEK.cipherW,
+	}
+
+	plainDEK, err = ibe.Decrypt(suite, publicKey, &dekSignature, &dek)
+	if err != nil {
+		return nil, fmt.Errorf("decrypt dek: %w", err)
+	}
+
+	return plainDEK, nil
+}
+
+// DecryptData decrypts the message with the specified data encryption key.
+func DecryptData(plainDEK []byte, cipherData []byte) (plainData []byte, err error) {
+	plainData, err = aeadDecrypt(plainDEK, cipherData)
+	if err != nil {
+		return nil, fmt.Errorf("decrypt data: %w", err)
+	}
+
+	return plainData, nil
+}
+
 // =============================================================================
-
-// networkInfo provides network and chain information.
-type networkInfo struct {
-	client client.Client
-	chain  *chain.Info
-}
-
-// retrieveNetworkInfo accesses the specified network for the specified chain
-// hash to extract information.
-func retrieveNetworkInfo(ctx context.Context, network string, chainHash string) (networkInfo, error) {
-	hash, err := hex.DecodeString(chainHash)
-	if err != nil {
-		return networkInfo{}, fmt.Errorf("decoding chain hash: %w", err)
-	}
-
-	client, err := dhttp.New(network, hash, transport())
-	if err != nil {
-		return networkInfo{}, fmt.Errorf("creating client: %w", err)
-	}
-
-	chain, err := client.Info(ctx)
-	if err != nil {
-		return networkInfo{}, fmt.Errorf("getting client information: %w", err)
-	}
-
-	ni := networkInfo{
-		client: client,
-		chain:  chain,
-	}
-
-	return ni, nil
-}
 
 // retrievePairingSuite returns the pairing suite to use.
 func retrievePairingSuite() (pairing.Suite, error) {
 	return bls.NewBLS12381Suite(), nil
 }
 
-// transport sets reasonable defaults for the connection.
-func transport() *http.Transport {
-	return &http.Transport{
-		Proxy: http.ProxyFromEnvironment,
-		DialContext: (&net.Dialer{
-			Timeout:   10 * time.Second,
-			KeepAlive: 5 * time.Second,
-		}).DialContext,
-		ForceAttemptHTTP2:     true,
-		MaxIdleConns:          2,
-		IdleConnTimeout:       5 * time.Second,
-		TLSHandshakeTimeout:   5 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
-	}
-}
-
 // calculateRound will generate the round information based on the specified duration.
-func calculateRound(duration time.Duration, ni networkInfo) (roundIDHash []byte, roundID uint64, err error) {
+func calculateRound(duration time.Duration, client client.Client) (roundIDHash []byte, roundID uint64, err error) {
 
 	// We need to get the future round number based on the duration. The following
 	// call will do the required calculations based on the network `period` property
 	// and return a uint64 representing the round number in the future. This round
 	// number is used to encrypt the data and will also be used by the decrypt function.
-	roundID = ni.client.RoundAt(time.Now().Add(duration))
+	roundID = client.RoundAt(time.Now().Add(duration))
 
 	h := sha256.New()
 	if _, err := h.Write(chain.RoundToBytes(roundID)); err != nil {
--- foundation/drnd/serialize.go
+++ foundation/drnd/serialize.go
@@ -17,18 +17,19 @@ type metadata struct {
 	chainHash string
 }
 
-// cipherDex represents the different parts of the Data Encryption Key.
-type dek struct {
+// cipherDEK represents the different parts of the encrypted Data Encryption
+// Key after encryption.
+type cipherDEK struct {
 	kyberPoint []byte
 	cipherV    []byte
 	cipherW    []byte
 }
 
-// fileInfo represents the different parts of the encrypted source.
-type fileInfo struct {
+// file represents the different parts of the encrypted source.
+type file struct {
 	metadata   metadata
-	dek        dek
-	cipherText []byte
+	cipherDEK  cipherDEK
+	cipherData []byte
 }
 
 // =============================================================================
@@ -79,17 +80,17 @@ func write(out io.Writer, cipherDEK *ibe.Ciphertext, cipherText []byte, md metad
 }
 
 // read the encrypted data into its different parts.
-func read(in io.Reader) (fileInfo, error) {
+func read(in io.Reader) (file, error) {
 	data, err := io.ReadAll(in)
 	if err != nil {
-		return fileInfo{}, fmt.Errorf("failed to read the data from source: %w", err)
+		return file{}, fmt.Errorf("failed to read the data from source: %w", err)
 	}
 
 	rr := bufio.NewReader(bytes.NewReader(data))
 	if string(data[:5]) == "-----" {
 		var block *pem.Block
 		if block, _ = pem.Decode(data); block == nil {
-			return fileInfo{}, fmt.Errorf("decoding PEM: %s", "block is nil")
+			return file{}, fmt.Errorf("decoding PEM: %s", "block is nil")
 		}
 
 		rr = bufio.NewReader(bytes.NewReader(block.Bytes))
@@ -97,53 +98,53 @@ func read(in io.Reader) (fileInfo, error) {
 
 	roundIDStr, err := readHeaderLine(rr)
 	if err != nil {
-		return fileInfo{}, fmt.Errorf("failed to read roundID: %w", err)
+		return file{}, fmt.Errorf("failed to read roundID: %w", err)
 	}
 
 	roundID, err := strconv.Atoi(roundIDStr)
 	if err != nil {
-		return fileInfo{}, fmt.Errorf("failed to convert round: %w", err)
+		return file{}, fmt.Errorf("failed to convert round: %w", err)
 	}
 
 	chainHash, err := readHeaderLine(rr)
 	if err != nil {
-		return fileInfo{}, fmt.Errorf("failed to read chain hash: %w", err)
+		return file{}, fmt.Errorf("failed to read chain hash: %w", err)
 	}
 
 	kyberPoint, err := readPayloadBytes(rr)
 	if err != nil {
-		return fileInfo{}, fmt.Errorf("failed to read kyber point: %w", err)
+		return file{}, fmt.Errorf("failed to read kyber point: %w", err)
 	}
 
 	cipherV, err := readPayloadBytes(rr)
 	if err != nil {
-		return fileInfo{}, fmt.Errorf("failed to read cipher v: %w", err)
+		return file{}, fmt.Errorf("failed to read cipher v: %w", err)
 	}
 
 	cipherW, err := readPayloadBytes(rr)
 	if err != nil {
-		return fileInfo{}, fmt.Errorf("failed to read cipher w: %w", err)
+		return file{}, fmt.Errorf("failed to read cipher w: %w", err)
 	}
 
-	cipherText, err := readPayloadBytes(rr)
+	cipherData, err := readPayloadBytes(rr)
 	if err != nil {
-		return fileInfo{}, fmt.Errorf("failed to read cipher text w: %w", err)
+		return file{}, fmt.Errorf("failed to read cipher text w: %w", err)
 	}
 
-	fi := fileInfo{
+	f := file{
 		metadata: metadata{
 			roundID:   uint64(roundID),
 			chainHash: chainHash,
 		},
-		dek: dek{
+		cipherDEK: cipherDEK{
 			kyberPoint: kyberPoint,
 			cipherV:    cipherV,
 			cipherW:    cipherW,
 		},
-		cipherText: cipherText,
+		cipherData: cipherData,
 	}
 
-	return fi, nil
+	return f, nil
 }
 
 // readPayloadBytes reads the section of the payload.
