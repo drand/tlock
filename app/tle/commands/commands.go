146a2b2a994683adea87e343284e85582f8888d8
gerep <daniel.gerep@protonmail.com>
2022-06-29 11:16:31 -0300

    add duration for days months and years

--- app/tle/commands/commands.go
+++ app/tle/commands/commands.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/drand/tlock/foundation/drnd"
@@ -34,15 +36,68 @@ func Encrypt(ctx context.Context, flags Flags, out io.Writer, in io.Reader) erro
 	}
 
 	if flags.Duration != "" {
-		duration, err := time.ParseDuration(flags.Duration)
+		duration, err := parseDuration(flags.Duration)
 		if err != nil {
 			return fmt.Errorf("parse duration: %w", err)
 		}
-
+		fmt.Println("duration", duration)
 		return drnd.EncryptWithDuration(ctx, out, in, network, aead, duration, flags.Armor)
 	}
 
 	return nil
+}
+
+// calculateDurationInHours parses the number and return the duration calculation
+// in hours.
+func calculateDurationInHours(number string, calc int) (time.Duration, error) {
+	i, err := strconv.Atoi(number)
+	if err != nil {
+		return time.Second, fmt.Errorf("calculate duration: %w", err)
+	}
+
+	return time.Duration(i*calc) * time.Hour, nil
+}
+
+// parseDuration tries to parse the duration, also considering days, years and
+// months
+func parseDuration(duration string) (time.Duration, error) {
+	d, err := time.ParseDuration(duration)
+	if err != nil {
+
+		// Check for a unit convertion error and try to parse.
+		if strings.Contains(err.Error(), "unknown unit") {
+
+			// We only accept d, m or y units.
+			if !strings.ContainsAny(duration, "dMy") {
+				return time.Second, fmt.Errorf("unknown unit")
+			}
+
+			// Days
+			number, _, found := strings.Cut(duration, "d")
+			if found {
+				return calculateDurationInHours(number, 24)
+			}
+
+			// Months
+			// Considering a month with 30 days
+			number, _, found = strings.Cut(duration, "M")
+			if found {
+				return calculateDurationInHours(number, 24*30)
+
+			}
+
+			// Years
+			// Considering a year with 365 days
+			number, _, found = strings.Cut(duration, "y")
+			if found {
+				return calculateDurationInHours(number, 24*365)
+			}
+		}
+
+		return time.Second, fmt.Errorf("parse duration: %w", err)
+	}
+
+	return d, nil
 }
 
 // Decrypt performs the decryption operation.
